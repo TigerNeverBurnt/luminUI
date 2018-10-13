67763578b3bf81600b37310530298727551dde63
hs267 <hs267@mail.missouri.edu>
2018-10-13 00:38:29 -0500

    add text area and title

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png">
     <div>
       <el-button @click="startHacking">Start</el-button>
-    </div>
+    </div> -->
+              <el-container style="height:100%; border: 1px solid #eee">
+            <el-aside width="600px" style="background-color: rgb(238, 241, 246)">
+              
+            </el-aside>
+            
+            <!-- <el-container> -->
+            <el-main>
+              <textTitle/>
+              
+
+            <div >
+          <textArea/>
+
+          </div>
+            </el-main>
+
+                
+
+              <!-- </el-container> -->
+            
+          </el-container>
+          
+    
   </div>
+  
 </template>
 
 <script>
+
+import textTitle from './textTitle.vue';
+import textArea from './textArea.vue';
+
 export default {
+  components:{
+    textTitle,
+    textArea
+  },
   methods: {
     startHacking () {
       this.$notify({
