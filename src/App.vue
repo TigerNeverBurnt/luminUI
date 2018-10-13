0a9e61872ea5191c19d17e4d3e78ad072704da3b
Hao Ding <dh@mwc-005023.dhcp.missouri.edu>
2018-10-13 01:15:00 -0500

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,15 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png">
     <div>
-      <el-button @click="startHacking">Start</el-button>
-    </div> -->
-              <el-container style="height:100%; border: 1px solid #eee">
-            <el-aside width="600px" style="background-color: rgb(238, 241, 246)">
-              
-            </el-aside>
-            
-            <!-- <el-container> -->
-            <el-main>
-              <textTitle/>
-              
+        <record ref="record"></record>
+    </div>
 
-            <div >
-          <textArea/>
-
-          </div>
-            </el-main>
-
-                
-
-              <!-- </el-container> -->
-            
-          </el-container>
-          
-    
-  </div>
-  
 </template>
 
 <script>
-
-import textTitle from './textTitle.vue';
-import textArea from './textArea.vue';
-
-export default {
-  components:{
-    textTitle,
-    textArea
-  },
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
+    import Record from './Record'
+    export default {
+        components: {
+            record: Record
+        },
     }
-  }
-}
 </script>
-
-<style>
-#app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import VueSpeech from 'vue-speech'
+import Record from './Record'
 
+Vue.use(VueSpeech)
 Vue.use(ElementUI)
-
+Vue.use(Record)
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App),
+    Record: h =>h(Record)
 })
