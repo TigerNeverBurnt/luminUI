68417baafbeb11ae41560e73c398749d5a01b429
hs267 <hs267@mail.missouri.edu>
2018-10-13 15:57:49 -0500

    update front ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
+
   <div id="app">
-    <el-container style="height:100%; border: 1px solid #eee">
-      <el-aside width="600px" style="background-color: rgb(238, 241, 246)">
-        <sd></sd>
-      </el-aside>
-      <el-main>
-          <div>
-              <Record ref="record"></Record>
-          </div>
-          <InputField/>
-      </el-main>  
+          <el-header>Tiger Hackkkkkkk</el-header>
+
+    <el-container style="height:700px; border: 1px solid #eee">
+      
+      <el-aside width="300px" style="background-color: rgb(238, 241, 246)">
+    <sd/>  <sd/><sd/><sd/><sd/>  <sd/>
+    
+
+  </el-aside>
+          
+            <el-container>
+
+              <el-main style="background-color: rgb(238, 241, 246)">
+                  <div>
+                      <Record ref="record"></Record>
+                  </div>
+                  <br><br>               
+
+                  <InputField/>
+              </el-main> 
+            </el-container>
+            
+              
+          
+
     </el-container>
+    <!-- <el-footer style="margin-bottom:0px; height:100%">Footer
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d99296.49783267855!2d-92.4010505011079!3d38.94647296871662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87dcb79505ed8739%3A0xc18305329c07fb48!2sUniversity+of+Missouri!5e0!3m2!1sen!2sus!4v1537897164099" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </el-footer> -->
+            
   </div>
 </template>
 
@@ -32,6 +52,12 @@ export default {
   text-align: center;
   
 }
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 100%;
+  }
 .type1 {
     width: 2%;
     height:2%;
@@ -39,6 +65,15 @@ export default {
     border-radius: 50%;
     border: 2px solid black;
 }
+ .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  body > .el-container {
+    margin-bottom: 40px;
+  }
 </style>
     
 
--- src/sd.vue
+++ src/sd.vue
@@ -1,23 +1,57 @@
 <template>
+
+
     <el-row>
-      <el-col :span="8" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
-        <el-card :body-style="{ padding: '0px' }">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/8/88/Bright_red_tomato_and_cross_section02.jpg" class="image">
-          <div style="padding: 14px;" class="cen">
-            <span>Tasty tomato</span>
-            <div class="bottom clearfix">
-              <el-button type="text" class="button">Save image</el-button>
-            </div>
-          </div>
+      <el-col :span="25" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card  shadow="always" >
+              <!-- <el-tooltip class="item"  effect="dark" content="Tasty tomato" placement="right-start">
+          <img src="https://www.vosizneias.com/wp-content/uploads/2018/10/AP18284573743989-725x483.jpg"   @click="open7" class="image">
+          
+          </el-tooltip> -->
+
+          <el-popover
+            placement="right-start"
+            title="Hurricane"
+            width="200"
+            trigger="hover"
+            content="content">
+          <img slot="reference" src="https://www.vosizneias.com/wp-content/uploads/2018/10/AP18284573743989-725x483.jpg"   @click="dialogFormVisible = true" class="image">
+
+            
+          </el-popover>
+
+        <el-dialog title="Images" :visible.sync="dialogFormVisible">
+              <img  src="https://www.w3schools.com/css/img_5terre_wide.jpg"   class="imageInbox">
+
+              <el-collapse v-model="activeNames" @change="handleChange">
+                <el-collapse-item title="Check the Location" name="2">
+                  <div>
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d99296.49783267855!2d-92.4010505011079!3d38.94647296871662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87dcb79505ed8739%3A0xc18305329c07fb48!2sUniversity+of+Missouri!5e0!3m2!1sen!2sus!4v1537897164099" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+                  </div>
+                </el-collapse-item>
+              </el-collapse>
+
+              <div slot="footer" class="dialog-footer">
+                  <el-button @click="dialogFormVisible = false">Cancel</el-button>
+                  <el-button type="primary" @click="dialogFormVisible = false">Save</el-button>
+                </div>
+        </el-dialog>
+
+         
+          
         </el-card>
       </el-col>
     </el-row>
+    
 </template>
+
 
 <script>
 
 export default {
   
+ 
   data: {
     
     labelPosition: 'right',
@@ -26,10 +60,62 @@ export default {
       region: '',
       type: ''
     }
+  },
+   data() {
+      return {
+        
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          
+        },
+        activeNames: ['1'],
+        formLabelWidth: '120px'
+      };
+    },
+  methods:{
+    handleChange(val) {
+        console.log(val);
+      },
+    open5() {
+        // this.$alert('<img src="https://upload.wikimedia.org/wikipedia/commons/8/88/Bright_red_tomato_and_cross_section02.jpg" class="imageInbox">', 'Picture', {
+        //   dangerouslyUseHTMLString: true,
+        //   center: true
+
+        // });
+        
+      }
+,
+      open7() {
+        this.$confirm('<img src="https://www.w3schools.com/css/img_5terre_wide.jpg" class="imageInbox">', 'images', {
+          dangerouslyUseHTMLString: true,
+
+          confirmButtonText: 'yes',
+          cancelButtonText: 'cancel',
+          // type: 'warning',
+          center: true
+        }).then(() => {
+          // this.$message({
+          //   type: 'success',
+          //   // message: '删除成功!'
+          // });
+        }).catch(() => {
+          // this.$message({
+          //   type: 'info',
+          //   // message: '已取消删除'
+          // });
+        });
+      }
+
   }
+ 
+  
+  
 }
 
+
 </script>
+
 
 <style>
 #app {
@@ -55,8 +141,21 @@ export default {
   }
 
   .image {
+    margin-left: auto;
+    margin-right: auto;
     width: 100%;
     display: block;
+     padding: 1px;
+  display: block;
+  max-width:230px;
+  max-height:230px;
+  width: auto;
+  height: auto;
+  }
+
+  .imageInbox{
+    max-width: 100%;
+    height: auto;
   }
 
   .clearfix:before,
@@ -73,5 +172,6 @@ export default {
 .elb{
   padding: 20;
 }
+
 </style>
 
