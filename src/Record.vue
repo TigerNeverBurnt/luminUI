261e9afb6c6d9fe4742ad36b37a8143a3d188a79
Hao Ding <dh@mwc-005023.dhcp.missouri.edu>
2018-10-13 19:58:02 -0500

    ...

--- src/Record.vue
+++ src/Record.vue
@@ -1,9 +1,13 @@
 <template>
     <div >
-        <button v-on:click="record = !record">
-            {{re}}
+        <button v-on:click="changeRecord">
+            {{st}}
         </button>
-        <vue-speech v-if="record" @onTranscriptionEnd="onEnd" hidden/>
+
+        <vue-speech v-if="record" @onTranscriptionEnd="onEnd" />
+
+        <br>
+        <textarea >{{text}}</textarea>
     </div>
 
 </template>
@@ -16,17 +20,36 @@
         data(){
             return {
                 record: false,
-                re: "Start record!",
-                test: true,
-                words:[]
+                words:[],
+                text: "",
+                st: "Start"
             }
 
         },
+        created:{
+            func(){
+                this.text = Vue.VueLocalStorage.get('text');
+            }
+        }
+        ,
         methods:{
+            changeRecord(){
+                this.record = !this.record;
+                if(this.record == true){
+                    this.st = "Stop";
+                }
+                else {
+                    this.st = "Start";
+                    this.$nextTick(() => {this.record = true});
+                    window.location.reload();
+
+                }
+            },
             onEnd ({ lastSentence, transcription }) {
                 this.words = transcription
-                alert(this.words)
-                this.lastSentence = lastSentence
+                this.text = this.text+lastSentence
+                Vue.VueLocalStorage.set('text', this.text);
+
             }
         }
     }
